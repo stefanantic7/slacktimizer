23526f38fb19e23d813963c6b2a16699e32f9367
Stefan Antic <stefan.antic@labs.devana.rs>
2016-08-31 13:02:07 +0200

    Deleted unnecessary lines from login and register views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,36 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+    <div>Login</div>
+    <form role="form" method="POST" action="{{ url('/login') }}">
+        {{ csrf_field() }}
+        <div {{ $errors->has('email') ? ' has-error' : '' }}>
+            <label for="email">E-Mail Address</label>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" autofocus>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" autofocus>
+            @if ($errors->has('email'))
+                <span><strong>{{ $errors->first('email') }}</strong></span>
+            @endif
+        </div>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+        <div {{ $errors->has('password') ? ' has-error' : '' }}>
+            <label for="password">Password</label>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+            <div>
+                <input id="password" type="password" class="form-control" name="password">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @if ($errors->has('password'))
+                    <span class="help-block"><strong>{{ $errors->first('password') }}</strong></span>
+                @endif
             </div>
         </div>
-    </div>
-</div>
+
+        <label>
+            <input type="checkbox" name="remember"> Remember Me
+        </label>
+
+        <button type="submit">Login</button>
+
+        <a href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+    </form>
 @endsection

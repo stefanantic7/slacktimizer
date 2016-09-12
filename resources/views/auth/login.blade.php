07d8c196a3b8fe8ac42efd5030c23259ae97635e
Stefan Antic <stefan.antic@labs.devana.rs>
2016-09-12 10:32:48 +0200

    Merge branch 'dev' of https://pmedia.codebasehq.com/slow-internet-optimizator/slack-optimizator into dev
    # Conflicts:
    #	composer.lock
    #	routes/web.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,41 +1,38 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
 
-    <div class="wrapper">
-        <h1>Login</h1><br/>
-        <form role="form" method="POST" action="{{ url('/login') }}">
-            {{ csrf_field() }}
+<h1>Login</h1><br/>
+<form role="form" method="POST" action="{{ url('/login') }}">
+    {{ csrf_field() }}
 
-            <label for="email">E-Mail Address</label>
+    <label for="email">E-Mail Address</label>
 
-            <input class="alignRight" id="email" type="email" name="email" value="{{ old('email') }}" autofocus>
-            <br/>
-            @if ($errors->has('email'))
-                <span><strong>{{ $errors->first('email') }}</strong></span>
-            @endif<br/>
+    <input class="alignRight" id="email" type="email" name="email" value="{{ old('email') }}" autofocus>
+    <br/>
+    @if ($errors->has('email'))
+        <span style="clear: both;"><strong>{{ $errors->first('email') }}</strong></span>
+    @endif<br/>
 
 
-            <label for="password">Password</label>
+    <label for="password">Password</label>
 
 
-                <input class="alignRight" id="password" type="password" name="password">
-                <br/>
-                @if ($errors->has('password'))
-                    <span><strong>{{ $errors->first('password') }}</strong></span>
-                @endif<br/>
+        <input class="alignRight" id="password" type="password" name="password">
+        <br/>
+        @if ($errors->has('password'))
+            <span><strong>{{ $errors->first('password') }}</strong></span>
+        @endif<br/>
 
 
 
 
-            <input type="checkbox" name="remember"> Remember Me <br/><br/>
+    <input type="checkbox" name="remember"> Remember Me <br/><br/>
 
 
-            <button type="submit">Login</button>
+    <button type="submit">Login</button>
 
-            <a class="alignRight" href="{{ url('/password/reset') }}">Forgot Your Password?</a><br/>
-            <a class="alignRight" href="/register">Register</a>
-        </form>
-    </div>
-</div>
+    <a class="alignRight" href="{{ url('/password/reset') }}">Forgot Your Password?</a><br/>
+    <a class="alignRight" href="/register">Register</a>
+</form>
+
 @endsection
--- resources/views/showMessage.blade.php
+++ resources/views/showMessage.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-
+    <div class="w">
 
     @foreach($json["messages"] as $message)
         <div class="textWrapper">
@@ -8,6 +8,7 @@
             <p class="text">{{$message['text']}}</p>
         </div>
     @endforeach
-    <textarea rows="4" class="reply" placeholder="Reply here!"></textarea>
-
+    <textarea rows="5" class="reply" placeholder="Reply here!"></textarea><br><br>
+    <input type="submit" value="Send!">
+    </div>
 @endsection

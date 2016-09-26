dcee232f80b0fe67f40a06c4dc3f520afda0033a
Milos Milosevic <milos.milosevic@labs.devana.rs>
2016-09-26 10:53:51 +0200

    Small changes added.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,20 @@
 @extends('layouts.app')
 
 @section('inner')
-    <div class="sideBarIcon" onclick="showSideBar()"><hr/><hr/><hr/>
+    <div class="sideBarIcon" onclick="showSideBar()">
+        <hr/>
+        <hr/>
+        <hr/>
     </div>
     <div class="sideBar">
         <div class="info">
-            <div class="teamName">{{Auth::user()->team_name}}</div>
+            <div class="teamName"><a href="{{url('/')}}">{{Auth::user()->team_name}}</a></div>
             <div class="mainUser">{{Auth::user()->name}}</div>
         </div>
         <div class="section">CHANNELS <span class="plus">+</span></div>
         @foreach($channels as $channel)
             <div class="channels"><a href="/channels/chat/{{$channel->chat_id}}">&#35{{$channel->name}}</a></div><br>
         @endforeach
-        {{--<select>--}}
-            {{--<option>1</option>--}}
-            {{--<option>2</option>--}}
-            {{--<option>3</option>--}}
-            {{--<option>4</option>--}}
-            {{--<option>5</option>--}}
-        {{--</select>--}}
         <div class="section">DIRECT MESSAGES <span class="plus">+</span></div>
         @foreach($ims as $im)
             <div class="channels"><a href="/ims/chat/{{$im->chat_id}}">&#64{{$im->username}}</a></div><br>
@@ -26,7 +22,7 @@
     </div>
     <div class="content">
         @if(empty($history))
-        <div class="welcome">Welcome</div>
+            <div class="welcome">Welcome</div>
         @endif
         <form method="post" action="{{url('/' . $chatId)}}">
             {{csrf_field()}}
@@ -35,7 +31,7 @@
                 <div class="sideBarMask" onclick="hideSideBar()"></div>
                 <p class="fullName">{{$chatName}}</p>
                 <a href="{{ url('/logout') }}" class="logout"
-                    onclick="event.preventDefault();
+                   onclick="event.preventDefault();
                      document.getElementById('logout-form').submit();">
                     Logout
                 </a>
@@ -47,16 +43,13 @@
                     <p class="message">{{$message['text']}}</p>
                 @endforeach
             </div>
-
-
+            @if($history)
             <div class="sendWrapper">
                 <textarea name="message" rows="1" class="newMessage"></textarea>
                 <input type="submit" value="Send" class="sendMessage">
             </div>
-
+            @endif
         </form>
-
-
     </div>
 
 @endsection

07f5ff4ac9fbaebf21895649f16b15aef584c28a
Milos Milosevic <milos.milosevic@labs.devana.rs>
2016-09-27 15:47:11 +0200

    Bug fixed.

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+function showSideBar() {
+    document.getElementsByClassName('sideBar')[0].style.visibility = 'visible';
+    document.getElementsByClassName('sideBarMask')[0].style.visibility='visible';
+}
+function hideSideBar() {
+    document.getElementsByClassName('sideBar')[0].style.visibility = 'hidden';
+    document.getElementsByClassName('sideBarMask')[0].style.visibility='hidden';
+}

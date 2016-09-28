63d76b8ba79b3f70b2510e1a897ec5a9e924d39a
Stefan Antic <stefan.antic@labs.devana.rs>
2016-09-28 15:37:28 +0200

    Fixed bugs.

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,1 @@
-function showSideBar() {
-    document.getElementsByClassName('sideBar')[0].style.visibility = 'visible';
-    document.getElementsByClassName('sideBarMask')[0].style.visibility='visible';
-}
-function hideSideBar() {
-    document.getElementsByClassName('sideBar')[0].style.visibility = 'hidden';
-    document.getElementsByClassName('sideBarMask')[0].style.visibility='hidden';
-}
+function showSideBar(){document.getElementsByClassName('sideBar')[0].style.visibility='visible';document.getElementsByClassName('sideBarMask')[0].style.visibility='visible'}function hideSideBar(){document.getElementsByClassName('sideBar')[0].style.visibility='hidden';document.getElementsByClassName('sideBarMask')[0].style.visibility='hidden'}
